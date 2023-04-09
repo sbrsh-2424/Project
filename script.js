e745593054e56cc226f040fdb839f6b6ff6e73fb
Sabarish T K <97340031+sbrsh-2424@users.noreply.github.com>
2023-04-09 18:12:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ var onButtonClick = function()
     var email = document.getElementById("email").value;
     var atPosition = email.indexOf("@");
     var dotPosition = email.lastIndexOf(".");
+    var gender = document.querySelector('input[name="gender"]:checked');
+    var cricket = document.getElementById("cricket");
+    var football = document.getElementById("football");
+    var others = document.getElementById("others");
 
-    if (name==null || name==" "){
+    if (name==null || name==""){
         message.textContent = "Name can't be blank";
     }
     else if (age < 7 || age > 60){
@@ -17,6 +21,12 @@ var onButtonClick = function()
     }
     else if (atPosition < 1 || dotPosition < atPosition+2 || dotPosition+2 >= email.length){
         message.textContent = "Please enter a valid e-mail address";
+    }
+    else if(gender==null){
+        message.textContent = "Choose your gender";
+    }
+    else if((cricket.checked || football.checked || others.checked) == false){
+        message.textContent = "Choose any of the sport"
     }
     else{
         message.textContent = "Information validated and found correct"
